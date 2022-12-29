c24f8baff95c35a4ac424b93bdb3c5ce5f5b05f8
VedadSiljic <siljicvedad17@gmail.com>
2022-12-29 14:26:49 +0100

    Upgrade syntax highligthing

--- highlighter.cpp
+++ highlighter.cpp
@@ -5,46 +5,62 @@ Highlighter::Highlighter(QTextDocument* parent) : QSyntaxHighlighter(parent) {
     HighlightingRule rule;
 
     keywordFormat.setForeground(QColor("#559bd4"));
-    const QString keywordPatterns[] = {
+    const QString keywordPatternsOne[] = {
 
         QStringLiteral("\\balignas\\b"), QStringLiteral("\\balignof\\b"), QStringLiteral("\\band\\b"),
         QStringLiteral("\\band_eq\\b"), QStringLiteral("\\basm\\b"), QStringLiteral("\\bauto\\b"),
         QStringLiteral("\\bbitand\b"), QStringLiteral("\\bbitor\\b"), QStringLiteral("\\bbool\\b"),
-        QStringLiteral("\\bbreak\\b"), QStringLiteral("\\bcase\\b"), QStringLiteral("\\bcatch\\b"),
-        QStringLiteral("\\bchar\\b"), QStringLiteral("\\bchar\\b"), QStringLiteral("\\bchar16_t\\b"),
-        QStringLiteral("\\bchar32_t\\b"), QStringLiteral("\\bclass\\b"), QStringLiteral("\\bcompl\\b"),
-        QStringLiteral("\\bconst\\b"), QStringLiteral("\\bconstexpr\\b"), QStringLiteral("\\bconst_cast\\b"),
-        QStringLiteral("\\bcontinue\\b"), QStringLiteral("\\bdecltype\\b"), QStringLiteral("\\bdefault\\b"),
-        QStringLiteral("\\bdelete\\b"), QStringLiteral("\\bdo\\b"), QStringLiteral("\\bdouble\\b"),
-        QStringLiteral("\\bdynamic_cast\\b"), QStringLiteral("\\belse\\b"), QStringLiteral("\\benum\\b"),
+        QStringLiteral("\\bcase\\b"), QStringLiteral("\\bcatch\\b"),QStringLiteral("\\bchar\\b"),
+        QStringLiteral("\\bchar\\b"), QStringLiteral("\\bchar16_t\\b"), QStringLiteral("\\bchar32_t\\b"),
+        QStringLiteral("\\bclass\\b"), QStringLiteral("\\bcompl\\b"), QStringLiteral("\\bconst\\b"),
+        QStringLiteral("\\bconstexpr\\b"), QStringLiteral("\\bconst_cast\\b"), QStringLiteral("\\bdecltype\\b"),
+        QStringLiteral("\\bdefault\\b"), QStringLiteral("\\bdelete\\b"), QStringLiteral("\\bdo\\b"),
+        QStringLiteral("\\bdouble\\b"), QStringLiteral("\\bdynamic_cast\\b"), QStringLiteral("\\benum\\b"),
         QStringLiteral("\\bexplicit\\b"), QStringLiteral("\\bexport\\b"), QStringLiteral("\\bextern\\b"),
-        QStringLiteral("\\bfalse\\b"), QStringLiteral("\\bfloat\\b"), QStringLiteral("\\bfor\\b"),
-        QStringLiteral("\\bfriend\\b"), QStringLiteral("\\bgoto\\b"), QStringLiteral("\\bif\\b"),
-        QStringLiteral("\\binline\\b"), QStringLiteral("\\bint\\b"), QStringLiteral("\\blong\\b"),
-        QStringLiteral("\\bmutable\\b"), QStringLiteral("\\bnamespace\\b"), QStringLiteral("\\bnew\\b"),
-        QStringLiteral("\\bnoexcept\\b"), QStringLiteral("\\bnot\\b"), QStringLiteral("\\bnot_eq\\b"),
-        QStringLiteral("\\bnullptr\\b"), QStringLiteral("\\boperator\\b"), QStringLiteral("\\bor\\b"),
-        QStringLiteral("\\bor_eq\\b"), QStringLiteral("\\bprivate\\b"), QStringLiteral("\\bprotected\\b"),
-        QStringLiteral("\\bpublic\\b"), QStringLiteral("\\bregister\\b"), QStringLiteral("\\breinterpret_cast\\b"),
-        QStringLiteral("\\breturn\\b"), QStringLiteral("\\bshort\\b"), QStringLiteral("\\bsigned\\b"),
+        QStringLiteral("\\bfalse\\b"), QStringLiteral("\\bfloat\\b"), QStringLiteral("\\bfriend\\b"),
+        QStringLiteral("\\bgoto\\b"), QStringLiteral("\\binline\\b"), QStringLiteral("\\bint\\b"),
+        QStringLiteral("\\blong\\b"), QStringLiteral("\\bmutable\\b"), QStringLiteral("\\bnamespace\\b"),
+        QStringLiteral("\\bnew\\b"), QStringLiteral("\\bnoexcept\\b"), QStringLiteral("\\bnot\\b"),
+        QStringLiteral("\\bnot_eq\\b"), QStringLiteral("\\bnullptr\\b"), QStringLiteral("\\boperator\\b"),
+        QStringLiteral("\\bor\\b"), QStringLiteral("\\bor_eq\\b"), QStringLiteral("\\bprivate\\b"),
+        QStringLiteral("\\bprotected\\b"), QStringLiteral("\\bpublic\\b"), QStringLiteral("\\bregister\\b"),
+        QStringLiteral("\\breinterpret_cast\\b"), QStringLiteral("\\bshort\\b"), QStringLiteral("\\bsigned\\b"),
         QStringLiteral("\\bsizeof\\b"), QStringLiteral("\\bstatic\\b"), QStringLiteral("\\bstatic_assert\\b"),
-        QStringLiteral("\\bstatic_cast\\b"), QStringLiteral("\\bstruct\\b"), QStringLiteral("\\bswitch\\b"),
-        QStringLiteral("\\btemplate\\b"), QStringLiteral("\\bthis\\b"), QStringLiteral("\\bthread_local\\b"),
-        QStringLiteral("\\bthrow\\b"), QStringLiteral("\\btrue\\b"), QStringLiteral("\\btry\\b"),
-        QStringLiteral("\\btypedef\\b"), QStringLiteral("\\btypeid\\b"), QStringLiteral("\\btypename\\b"),
-        QStringLiteral("\\bunion\\b"), QStringLiteral("\\bunsigned\\b"), QStringLiteral("\\busing\\b"),
-        QStringLiteral("\\bvirtual\\b"), QStringLiteral("\\bvoid\\b"), QStringLiteral("\\bvolatile\\b"),
-        QStringLiteral("\\bwchar_t\\b"), QStringLiteral("\\bwhile\\b"), QStringLiteral("\\bxor\\b"),
-        QStringLiteral("\\bxor_eq\\b")
+        QStringLiteral("\\bstatic_cast\\b"), QStringLiteral("\\bstruct\\b"), QStringLiteral("\\btemplate\\b"),
+        QStringLiteral("\\bthis\\b"), QStringLiteral("\\bthread_local\\b"), QStringLiteral("\\bthrow\\b"),
+        QStringLiteral("\\btrue\\b"), QStringLiteral("\\btry\\b"), QStringLiteral("\\btypedef\\b"),
+        QStringLiteral("\\btypeid\\b"), QStringLiteral("\\btypename\\b"), QStringLiteral("\\bunion\\b"),
+        QStringLiteral("\\bunsigned\\b"), QStringLiteral("\\busing\\b"), QStringLiteral("\\bvirtual\\b"),
+        QStringLiteral("\\bvoid\\b"), QStringLiteral("\\bvolatile\\b"), QStringLiteral("\\bwchar_t\\b"),
+        QStringLiteral("\\bxor\\b"), QStringLiteral("\\bxor_eq\\b")
+
     };
 
-    for (const QString& pattern : keywordPatterns) {
+    for (const QString& pattern : keywordPatternsOne) {
 
         rule.pattern = QRegularExpression(pattern);
         rule.format = keywordFormat;
         highlightingRules.append(rule);
 
     }
+
+
+    keywordFormat.setForeground(QColor("#d39bd2"));
+    const QString keywordPatternsTwo[] = {
+        QStringLiteral("\\bbreak\\b"), QStringLiteral("\\bcontinue\\b"), QStringLiteral("\\belse\\b"),
+        QStringLiteral("\\bfor\\b"), QStringLiteral("\\bif\\b"), QStringLiteral("\\breturn\\b"),
+        QStringLiteral("\\bswitch\\b"), QStringLiteral("\\bwhile\\b")
+
+    };
+
+    for (const QString& pattern : keywordPatternsTwo) {
+
+        rule.pattern = QRegularExpression(pattern);
+        rule.format = keywordFormat;
+        highlightingRules.append(rule);
+
+    }
+
 
 
     classFormat.setFontWeight(QFont::Bold);
